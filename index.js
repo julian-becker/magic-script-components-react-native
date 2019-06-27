6cfdada9d88353e5e3f2435ef17de4585d86e3ef
m.foksinski <m.foksinski@nomtek.com>
2019-06-27 14:10:06 +0200

    add the TreeScript renderer in the exports

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import ARView from './ARView';
 import ARKitScript from './components/arkit-script/arkit-script';
+import ThreeScript from './web/renderer/three-script';
 
 export {
   ARView,
   ARKitScript,
+  ThreeScript
 };
